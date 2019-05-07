2b9495f0f39c7550e3e5ab00e125f1be77a48972
Cam Murphy <CamMurphy195@gmail.com>
2019-05-06 21:28:26 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Cams Horror Quiz
+      </h1>
+      <h2 class="subtitle">
+        What's Your Monster?
+      </h2>
+      <div class="links">
+        <router-link to="/questions" target="_blank" class="button--green"
+          >Get Started</router-link
+        >
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style>
+.container {
+  background-color: black;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #f1802d;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/questions.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>test</h1>
+    </div>
+  </section>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 25vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
